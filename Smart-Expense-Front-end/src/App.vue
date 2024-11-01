e1a86a20c5050961dfe34f8d2b88c564bab1b397
Liêu <92929538+TuongTheLieu@users.noreply.github.com>
2024-11-01 23:24:35 +0700

    Add files via upload

---
+++ Smart-Expense-Front-end/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+      <div
+        class="preloader flex-column justify-content-center align-items-center"
+      >
+        <img
+          class="animation__shake"
+          src="@/assets/logo-Smart-Expense.png"
+          alt="AdminLTELogo"
+          height="60"
+          width="60"
+        />
+      </div>
+      <component :is="$route.meta.layout || 'div'"></component>
+    </div>
+  </body>
+</template>
+
+
---
+++ Smart-Expense-Front-end/src/components/methods/MethodComponent.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  methods: {
+    calculateDisplayedPages(currentPage, totalPages) {
+      //Hàm hiển thị thanh phân trang
+      const maxDisplayedPages = 5;
+      const middlePage = Math.ceil(maxDisplayedPages / 2);
+      let startPage = currentPage - middlePage + 1;
+      if (startPage < 1) {
+        startPage = 1;
+      }
+      let endPage = startPage + maxDisplayedPages - 1;
+      if (endPage > totalPages) {
+        endPage = totalPages;
+        startPage = Math.max(1, endPage - maxDisplayedPages + 1);
+      }
+      return Array.from(
+        { length: endPage - startPage + 1 },
+        (_, i) => startPage + i
+      );
+    },
+  },
+};
+</script>
